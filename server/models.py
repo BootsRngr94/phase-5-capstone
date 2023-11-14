d4826f10a7bcdfbdbe920c11cc4625b136609b77
Luis Westberry <takuame7@gmail.com>
2023-11-14 13:54:17 -0600

    added models, their framework, and relationships/validations/serializations

--- server/models.py
+++ server/models.py
@@ -12,3 +12,73 @@ db = SQLAlchemy(metadata=metadata)
 
 # Models go here!
 #Main models are games, genres, consolegames, consoles. May add in user_games and users in the future.
+
+class Technician(db.Model, SerializerMixin):
+    __tablename__='technicians'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    #serialize rules and relationships below, is serialization nessecary? 
+    serialize_rules = ('-password', '-clients.technicians')
+    clients = db.relationship('Client', back_populates='technicians')
+    #validations
+    @validates('username')
+    def validate_username(self, key, username):
+        if type (username) == str:
+            return username
+        else: 
+            raise ValueError('Username must contain a string!')
+    @validates('password')
+    def validate_password(self, key, password):
+        if type (password) == str:
+            return password
+        else:
+            raise ValueError('Password must conatin a string!')
+    def __repr__(self):
+        return f'<Technicians {self.id}: {self.username}>'
+
+
+
+class Client(db.Model, SerializerMixin):
+    __tablename__='clients'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    phone = db.Column(db.String)
+    email = db.Column(db.String)
+    #foreignkey
+    technicians_id = db.Column(db.Integer, db.ForeignKey('technicians.technicians_id'))
+    #serialize_rules
+    serialize_rules = ('-technicians.clients', '-pools.client')  
+    #relationships
+    technicians = db.relationship('Technician', back_populates='clients', lazy='subquery')
+    pools = db.relationship('Pool', back_populates='client', cascade='all, delete-orphan')
+
+class Pool(db.Model):
+    __tablename__ = 'pools'
+    pools_id = db.Column(db.Integer, primary_key=True)
+    pools_location = db.Column(db.String)
+    pools_size = db.Column(db.String)
+    pools_condition_last_check = db.Column(db.String)
+    #foreignkey
+    clients_id = db.Column(db.Integer, db.ForeignKey('clients.clients_id'))
+    #serialize_rules
+    serialize_rules = ('-client.pools', '-pool_visits.pool')
+    #relationships
+    client = db.relationship('Client', back_populates='pools')
+    pool_visits = db.relationship('PoolVisit', back_populates='pool', cascade='all, delete-orphan')
+
+class PoolVisit(db.Model):
+    __tablename__ = 'pool_visits'
+    visits_id = db.Column(db.Integer, primary_key=True)
+    visits_notes = db.Column(db.String)
+    visits_FILTER_PSI = db.Column(db.Integer)
+    visits_PH_record = db.Column(db.Integer)
+    visits_CHL_record = db.Column(db.Integer)
+    visits_CHEMS_USED_record = db.Column(db.Integer)
+    serialize_rules = ('-technicians.pool_visits' ,'-pool.pool_visits')
+
+    technicians_id = db.Column(db.Integer, db.ForeignKey('technicians.technicians_id'))
+    technicians = db.relationship('Technicians', back_populates='pool_visits')
+    
+    pools_id = db.Column(db.Integer, db.ForeignKey('pools.pools_id'))
+    pool = db.relationship('Pool', back_populates='pool_visits')
