efd8224589526156e15104115d263e4a1570b0fe
Luis Westberry <takuame7@gmail.com>
2023-11-15 10:29:20 -0600

    set up seed framework

--- server/seed.py
+++ server/seed.py
@@ -8,4 +8,68 @@ from faker import Faker
 
 # Local imports
 from app import app  # Make sure 'app' is properly imported
+from models import db, Technician, Client, Pool, PoolVisit
 
+if __name__ == '__main__':
+    fake = Faker()
+    with app.app_context():
+        print("Spring cleaning the database!")
+        #Delete all db records
+        db.session.query(Technician).delete()
+        db.session.query(Client).delete()
+        db.session.query(Pool).delete()
+        db.session.query(PoolVisit).delete()
+
+        print("Datsbase initializing...")
+
+        print('Logging techs...')
+        technicians = [
+            Technician(),
+            Technician(),
+            
+        ]
+        db.session.add_all(technicians)
+        db.session.commit()
+
+        print('Setting up Clients...')
+        clients = [
+            Client(),
+            Client(),
+            Client(),
+            Client(),
+            Client(),
+            Client(),
+            
+
+        ]
+        db.session.add_all(clients)
+        db.session.commit()
+
+        print('Filling Pools...')
+        pools = [
+            Pool(),
+            Pool(),
+            Pool(),
+            Pool(),
+            Pool(),
+            Pool(),
+            
+            
+        ]
+        db.session.add_all(pools)
+        db.session.commit()
+
+        pool_visits = [
+            PoolVisit(),
+            PoolVisit(),
+            PoolVisit(),
+            PoolVisit(),
+            PoolVisit(),
+            PoolVisit(),
+            
+        ]
+        db.session.add_all(pool_visits)
+        db.session.commit()
+
+        print('Everything accounted for...')
+        print('Lets get it done!')
