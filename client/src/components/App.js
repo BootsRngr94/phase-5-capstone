f723066a748083154ca9406c4a7537e8fd18f55f
Luis Westberry <takuame7@gmail.com>
2023-11-17 14:47:35 -0600

    added skeleton componenets to REACT frontend, sorted further issues with backend. All GET methods for pools/clients/technicians functional. Pool_visits will not get- further testing on that one

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+
+
+
+
+
+export default App;
--- server/app.py
+++ server/app.py
@@ -32,67 +32,67 @@ def technicians():
         print(form_data)
     return response
     
-# #clients route base set up
-# @app.route('/clients', methods=['GET', 'POST'])
-# def clients():
-#     if request.method == 'GET':
-#         clients = Client.query.all()
-#         clients_dict = [client.to_dict(rules=('-clients',))for client in clients]
-#         response = make_response(clients_dict, 200)
-#     # elif request.method == 'POST':
-#     #     form_data = request.get_json()
-#     #     print(form_data)
-#         return response
+#clients route base set up
+@app.route('/clients', methods=['GET', 'POST'])
+def clients():
+    if request.method == 'GET':
+        clients = Client.query.all()
+        clients_dict = [client.to_dict(rules=('-clients',))for client in clients]
+        response = make_response(clients_dict, 200)
+    # elif request.method == 'POST':
+    #     form_data = request.get_json()
+    #     print(form_data)
+        return response
 
-# #pools route base set up
-# @app.route('/pools', methods=['GET', 'POST'])
-# def pools():
-#     if request.method == 'GET':
-#         pools = Pool.query.all()
-#         pools_dict = [pool.to_dict(rules=('-pools',))for pool in pools]
-#         response = make_response(pools_dict, 200)
-#     # elif request.method == 'POST':
-#     #     form_data = request.get_json()
-#     #     print(form_data)
-#         return response
+#pools route base set up
+@app.route('/pools', methods=['GET', 'POST'])
+def pools():
+    if request.method == 'GET':
+        pools = Pool.query.all()
+        pools_dict = [pool.to_dict(rules=('-pools',))for pool in pools]
+        response = make_response(pools_dict, 200)
+    # elif request.method == 'POST':
+    #     form_data = request.get_json()
+    #     print(form_data)
+        return response
     
 #pool_visits base route set up, needs PATCH included for user to change data
-# @app.route('/pool_visits', methods=['POST'])
-# def create_pool_visit():
-#     if request.method == 'POST':
-#         try:
-#             data = request.get_json()
-#             new_pool_visit = PoolVisit(**data)
-#             db.session.add(new_pool_visit)
-#             db.session.commit()
-#             return jsonify(new_pool_visit.to_dict()), 201
-#         except Exception as e:
-#             return jsonify({'error': str(e)}), 400
+@app.route('/pool_visits', methods=['POST'])
+def create_pool_visit():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            new_pool_visit = PoolVisit(**data)
+            db.session.add(new_pool_visit)
+            db.session.commit()
+            return jsonify(new_pool_visit.to_dict()), 201
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
 
-# @app.route('/pool_visits/<int:visit_id>', methods=['PATCH', 'DELETE'])
-# def update_or_delete_pool_visit(visit_id):
-#     pool_visit = PoolVisit.query.get(visit_id)
+@app.route('/pool_visits/<int:visit_id>', methods=['PATCH', 'DELETE'])
+def update_or_delete_pool_visit(visit_id):
+    pool_visit = PoolVisit.query.get(visit_id)
 
-#     if not pool_visit:
-#         return jsonify({'error': 'Pool visit not found'}), 404
+    if not pool_visit:
+        return jsonify({'error': 'Pool visit not found'}), 404
 
-#     if request.method == 'PATCH':
-#         try:
-#             data = request.get_json()
-#             for key, value in data.items():
-#                 setattr(pool_visit, key, value)
-#             db.session.commit()
-#             return jsonify(pool_visit.to_dict()), 200
-#         except Exception as e:
-#             return jsonify({'error': str(e)}), 400
+    if request.method == 'PATCH':
+        try:
+            data = request.get_json()
+            for key, value in data.items():
+                setattr(pool_visit, key, value)
+            db.session.commit()
+            return jsonify(pool_visit.to_dict()), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
 
-#     elif request.method == 'DELETE':
-#         try:
-#             db.session.delete(pool_visit)
-#             db.session.commit()
-#             return jsonify({'message': 'Pool visit deleted successfully'}), 200
-#         except Exception as e:
-#             return jsonify({'error': str(e)}), 500
+    elif request.method == 'DELETE':
+        try:
+            db.session.delete(pool_visit)
+            db.session.commit()
+            return jsonify({'message': 'Pool visit deleted successfully'}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
 
 
     
